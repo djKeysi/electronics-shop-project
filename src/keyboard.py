91a91dfb60536f3f6393376d4a730514946cb349
djkeysi <evgeniyzenchenko@gmail.com>
2023-06-29 22:18:43 +0700

    end work homework-5 dorabotka

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,31 +3,27 @@ class MixKeyboard:
 
     #ID = 0
     def __init__(self):
-        self.language = "RU"
-        #MixKeyboard.ID +=1
+        self.__language = "EN"
 
-        #if MixKeyboard.ID == 1:
-            #self.language = "RU"
-        #elif MixKeyboard.ID == 2:
-            #self.language = "RU"
+    def change_lang(self):
+        self.__language = "RU"
+        return self
+    @property
+    def language(self): # для ошибки AttributeError: property 'language' of 'KeyBoard' object has no setter
+        return self.__language
+
+
+
 
 class KeyBoard(MixKeyboard):
     def __init__(self,name, price, quantity):
+        super().__init__()
         self.name = name
         self.price = price
         self.quantity = quantity
-        self.language = "EN"
-        #self.__language = "EN"  # для ошибки AttributeError: property 'language' of 'KeyBoard' object has no setter
 
-
-    #@property
-    #def language(self): # для ошибки AttributeError: property 'language' of 'KeyBoard' object has no setter
-        #return self.__language
     def __str__(self):
         return self.name
-    def change_lang(self):
-        super().__init__()
-        return self
 
 
 
